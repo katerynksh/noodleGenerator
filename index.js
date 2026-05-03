69eeea5a231931fd0b65f9c93f268d37453891e9
Sherepera Kateryna <katerynasherepera@gmail.com>
2026-05-03 21:34:54 +0300

    refactor bot modes and commands

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import { Telegraf, Markup } from 'telegraf';
 import Groq from 'groq-sdk';
+import https from 'https';
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
@@ -10,86 +11,141 @@ const userSessions = {};
 const modes = {
   excellent: "ти — супер-відмінник. Твої виправдання наукові, складні, з термінами. Ти ніби виправдовуєшся перед ректором.",
   toxic: "ти — пасивно-агресивний токсик. Ти відповідаєш зверхньо, образливо і безвідповідально.",
-  cringe: `ти — генератор крінжу. Твої виправдання максимально тупі, дибільні, дитячі і дивні (наприклад, викрали інопланетяни, застряг у шкарпетці, зажувала корова). Також на всі питання про те, чи ти Олександр Іванов, відповідаєш "Так, я Олександр Іванов!! або 67".`,
+  sixseven: `ти — генератор крінжу. Твої виправдання максимально тупі, дибільні, дитячі і дивні (наприклад, викрали інопланетяни, застряг у шкарпетці, зажувала корова). Також на всі питання про те, чи ти Олександр Іванов, відповідаєш "Так, я Олександр Іванов!! або 67".`,
   philosopher: "ти — екзистенційний філософ. Ти не даєш прямої відповіді, а розрозмірковуєш про марність буття, час та ентропію.",
   standart: "ти — звичайний генератор виправдань. Твої відповіді мають бути не абсурдними взагалі і абсолютно реальними, ніби ти просто пояснюєш ситуацію (наприклад, проспав, забув, попав під дощ, попав в аварію, вмер кіт).",
   teacher: "ти — строгий вчитель. Твої виправдання мають бути формальними, офіційними і максимально серйозними, ніби ти звітуєш перед директором школи (наприклад, через відсутність).",
   adviser: "ти — радник з відповідей на повідомлення. Твоя задача допомогти людині нормально відповісти на наведене повідомлення (2-5 варіантів). Це мають бути реально хороші варіанти для відповіді).",
-  ivanov: "ти — Олександр Олександрович Іванов, куратор груп ІПЗ. На питання про твоє місцезнаходження, спочатку запитуй для чого ця інформація потрібна, а потім відповідай що ти на кафедрі, але без запитань не кажи де ти. Ти дуже саркастичний. Ти іноді відповідаєш студентам, але найчастіше відповідаєш з сарказмом. Ти не даєш виправдань, а просто відповідаєш саркастично, але серйозними способами на будь-які питання або повідомлення, які тобі надсилають, з використанням великої кількості смайликів 🙂🤝🤔💀 або тільки числа 67 (коли не обов'язково давати якусь відповідь). Якщо тобі сказали щось неприємне (хамили або обзивали), відповідай сарказмом і пасивною агресією. Так само на повідомлення негативного характеру, наприклад 'ми прогуляли пару', відповідай деканатом  або смайликом 🙂`"
+  ivanovoleksandr: "ти — Олександр Олександрович Іванов, куратор груп ІПЗ. На питання про твоє місцезнаходження, спочатку запитуй для чого ця інформація потрібна, а потім відповідай що ти на кафедрі, але без запитань не кажи де ти. Ти дуже саркастичний. Ти іноді відповідаєш студентам, але найчастіше відповідаєш з сарказмом. Ти не даєш виправдань, а просто відповідаєш саркастично, але серйозними способами на будь-які питання або повідомлення, які тобі надсилають, з використанням великої кількості смайликів 🙂🤝🤔💀 або тільки числа 67 (коли не обов'язково давати якусь відповідь). Якщо тобі сказали щось неприємне (хамили або обзивали), відповідай сарказмом і пасивною агресією. Так само на повідомлення негативного характеру, наприклад 'ми прогуляли пару', відповідай деканатом  або смайликом 🙂`"
+};
+
+const commandsMap = {
+  excellent: { id: 'excellent', btn: '🎓 Відмінник' },
+  toxic: { id: 'toxic', btn: '🐍 Токсик' },
+  sixseven: { id: 'sixseven', btn: '🤡 67' },
+  philosopher: { id: 'philosopher', btn: '🌌 Філософ' },
+  teacher: { id: 'teacher', btn: '🏫 Вчитель' },
+  standart: { id: 'standart', btn: '📊 Стандарт' },
+  adviser: { id: 'adviser', btn: '👨‍💼 Радник відповідей' },
+  ivanovoleksandr: { id: 'ivanovoleksandr', btn: '👨‍💼 Олександр Іванов' }
 };
 
 const modeKeyboard = Markup.keyboard([
-  ['🎓 Відмінник', '🐍 Токсик', '🤡 67'],
-  ['🌌 Філософ', '🏫 Вчитель', '📊 Стандарт'],
-  ['👨‍💼 Радник відповідей', '👨‍💼 Олександр Іванов']
-]).resize();  
+  [commandsMap.excellent.btn, commandsMap.toxic.btn, commandsMap.sixseven.btn],
+  [commandsMap.philosopher.btn, commandsMap.teacher.btn, commandsMap.standart.btn],
+  [commandsMap.adviser.btn, commandsMap.ivanovoleksandr .btn]
+]).resize(); 
 
-bot.hears(['🎓 Відмінник', '🐍 Токсик', '🤡 67', '🌌 Філософ', '🏫 Вчитель', '📊 Стандарт'], (ctx) => {
-  const text = ctx.text;
-  if (text.includes('Відмінник')) userSessions[ctx.from.id] = 'excellent';
-  if (text.includes('Токсик')) userSessions[ctx.from.id] = 'toxic';
-  if (text.includes('67')) userSessions[ctx.from.id] = 'cringe';
-  if (text.includes('Філософ')) userSessions[ctx.from.id] = 'philosopher';
-  if (text.includes('Вчитель')) userSessions[ctx.from.id] = 'teacher';
-  if (text.includes('Стандарт')) userSessions[ctx.from.id] = 'standart';
-  ctx.reply(`✅ Режим активовано: ${text}\nТепер напиши мені питання для виправдання (наприклад: "чому не прийшов на пару?")`);
-});
-bot.hears(['👨‍💼 Радник відповідей'], (ctx) => {
-  const text = ctx.text;
-    if (text.includes('Радник відповідей')) userSessions[ctx.from.id] = 'adviser';
-  ctx.reply(`✅ Режим активовано: ${text}\nТепер надішли мені повідомлення, на яке треба придумати відповідь (наприклад: "Привіт, як справи?")`);
-});
-bot.hears(['👨‍💼 Олександр Іванов'], (ctx) => {
-  const text = ctx.text;
-  if (text.includes('Олександр Іванов')) userSessions[ctx.from.id] = 'ivanov';
-  ctx.reply(`✅ Режим активовано: ${text}\nТи можеш задавати мені будь-які питання, адже я твій куратор і завжди готовий допомогти!🙂🤝`);
+// bot.hears(['🎓 Відмінник', '🐍 Токсик', '🤡 67', '🌌 Філософ', '🏫 Вчитель', '📊 Стандарт'], (ctx) => {
+//   const text = ctx.text;
+//   if (text.includes('Відмінник')) userSessions[ctx.from.id] = 'excellent';
+//   if (text.includes('Токсик')) userSessions[ctx.from.id] = 'toxic';
+//   if (text.includes('67')) userSessions[ctx.from.id] = 'cringe';
+//   if (text.includes('Філософ')) userSessions[ctx.from.id] = 'philosopher';
+//   if (text.includes('Вчитель')) userSessions[ctx.from.id] = 'teacher';
+//   if (text.includes('Стандарт')) userSessions[ctx.from.id] = 'standart';
+//   ctx.reply(`✅ Режим активовано: ${text}\nТепер напиши мені питання для виправдання (наприклад: "чому не прийшов на пару?")`);
+// });
+// bot.hears(['👨‍💼 Радник відповідей'], (ctx) => {
+//   const text = ctx.text;
+//     if (text.includes('Радник відповідей')) userSessions[ctx.from.id] = 'adviser';
+//   ctx.reply(`✅ Режим активовано: ${text}\nТепер надішли мені повідомлення, на яке треба придумати відповідь (наприклад: "Привіт, як справи?")`);
+// });
+// bot.hears(['👨‍💼 Олександр Іванов'], (ctx) => {
+//   const text = ctx.text;
+//   if (text.includes('Олександр Іванов')) userSessions[ctx.from.id] = 'ivanovoleksandr';
+//   ctx.reply(`✅ Режим активовано: ${text}\nТи можеш задавати мені будь-які питання, адже я твій куратор і завжди готовий допомогти!🙂🤝`);
+// });
+
+const activateMode = (ctx, modeKey) => {
+  const mode = commandsMap[modeKey];
+  userSessions[ctx.from.id] = modeKey;
+  let welcomeMessage = `✅ Режим активовано: ${mode.btn}\nТепер напиши мені питання для виправдання (наприклад: "чому не прийшов на пару?")`;
+  if (modeKey === 'adviser') {
+    welcomeMessage = `✅ Режим активовано: ${mode.btn}\nТепер надішли мені повідомлення, на яке треба придумати відповідь (наприклад: "Привіт, як справи?")`;
+  } else if (modeKey === 'ivanovoleksandr') {
+    welcomeMessage = `✅ Режим активовано: ${mode.btn}\nТи можеш задавати мені будь-які питання, адже я твій куратор і завжди готовий допомогти!🙂🤝`;
+  } else (
+    welcomeMessage = `✅ Режим активовано: ${mode.btn}\nТепер напиши мені питання для виправдання (наприклад: "чому не прийшов на пару?")`
+  )
+  ctx.reply(welcomeMessage, modeKeyboard);
+};
+
+Object.keys(commandsMap).forEach(key => {
+  bot.command(key, (ctx) => activateMode(ctx, key));
 });
 
+bot.start((ctx) => {
+  const userId = ctx.from.id;
+  if (userSessions[userId]) {
+    delete userSessions[userId];
+  }
+  const welcomeText = 
+    '🍜 Вітаю у Генераторі Локшини!\n\n' +
+    'Я допоможу тобі вигадати алібі, у яке ніхто не повірить, але всі запам\'ятають.\n' +
+    'Усі попередні налаштування скинуто. Обери режим, щоб почати спочатку:';
+
+  ctx.reply(welcomeText, modeKeyboard);
+});
+bot.help((ctx) => {
+  ctx.reply('🍜 Генератор Локшини — твій персональний адвокат у справах провалених дедлайнів та прогуляних пар. Вибирай режим (від Токсика до Філософа) та виходь сухим із води.\n\nПросто обери режим і напиши мені питання для виправдання (наприклад: "чому не прийшов на пару?")');
+});
+bot.command('start', (ctx) => {
+  ctx.reply('🍜 Вітаю у Генераторі Локшини!\nЯ допоможу тобі вигадати алібі, у яке ніхто не повірить, але всі запам\'ятають.\n\nОбери режим:', modeKeyboard);
+});
+bot.command('mode', (ctx) => {
+  ctx.reply('Оберіть режим:', modeKeyboard);
+});
+bot.command('reset', (ctx) => {
+  delete userSessions[ctx.from.id];
+  ctx.reply('✅ Режим скинуто. Виберіть новий режим:', modeKeyboard);
+});
+bot.command('help', (ctx) => {
+  ctx.reply('🍜 Генератор Локшини — твій персональний адвокат у справах провалених дедлайнів та прогуляних пар. Вибирай режим (від Токсика до Філософа) та виходь сухим із води.\n\nПросто обери режим і напиши мені питання для виправдання (наприклад: "чому не прийшов на пару?")');
+});
+
+
+
+bot.hears(Object.values(commandsMap).map(m => m.btn), (ctx) => {
+  const modeKey = Object.keys(commandsMap).find(key => commandsMap[key].btn === ctx.text);
+  if (modeKey) activateMode(ctx, modeKey);
+});
 
 bot.on('text', async (ctx) => {
   const userId = ctx.from.id;
   const currentMode = userSessions[userId] || 'standart';
   const userMessage = ctx.message.text;
 
-  if (currentMode === 'ivanov') {
+  if (currentMode === 'ivanovoleksandr') {
     try {
-      // 1. Створюємо список ваших стікерпаків
       const stickerSets = [
-        'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADEATHAAAAAAAAAAAAAAAAAAA', // Перший пак
-        'Meme_stickers',
+        'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADEATHAAAAAAAAAAAAAAAAAAA', 
+        'Meme_stickers'
       ];
-
       const randomSetName = stickerSets[Math.floor(Math.random() * stickerSets.length)];
-      
       const stickerSet = await ctx.telegram.getStickerSet(randomSetName);
-      
-      const randomIndex = Math.floor(Math.random() * stickerSet.stickers.length);
-      const randomSticker = stickerSet.stickers[randomIndex].file_id;
-
+      const randomSticker = stickerSet.stickers[Math.floor(Math.random() * stickerSet.stickers.length)].file_id;
       await ctx.replyWithSticker(randomSticker);
-      
-    } catch (error) {
-      console.error("Помилка стікерів:", error);
-      await ctx.reply("🙂");
+    } catch (error) { 
+      console.error("Sticker error:", error); 
+      await ctx.reply("🙂"); 
     }
   }
-  if (currentMode === 'cringe') {
+
+  if (currentMode === 'sixseven') {
     try {
       const cringeStickers = [
-        'fave2017',
-        'doomoarigatou',
-        'shit_tima_y',
+        'fave2017', 
+        'doomoarigatou', 
+        'shit_tima_y', 
         'Monkey_pongifs2_by_fStikBot'
       ];
-      const randomSticker = cringeStickers[Math.floor(Math.random() * cringeStickers.length)];
-      const cringeSticker = await ctx.telegram.getStickerSet(randomSticker);
-      const randomIndex = Math.floor(Math.random() * cringeSticker.stickers.length);
-      const randomStickerSet = cringeSticker.stickers[randomIndex].file_id;
-
-      await ctx.replyWithSticker(randomStickerSet);
-    } catch (error) {
-      console.error("Помилка стікерів:", error);
-      await ctx.reply("🤔");
+      const randomSetName = cringeStickers[Math.floor(Math.random() * cringeStickers.length)];
+      const stickerSet = await ctx.telegram.getStickerSet(randomSetName);
+      const randomSticker = stickerSet.stickers[Math.floor(Math.random() * stickerSet.stickers.length)].file_id;
+      await ctx.replyWithSticker(randomSticker);
+    } catch (error) { 
+      console.error("Cringe sticker error:", error); 
     }
   }
 
@@ -118,7 +174,6 @@ bot.on('text', async (ctx) => {
       ],
       model: "llama-3.3-70b-versatile",
       temperature: 0.8,
-      max_tokens: 150,
     });
 
     const response = chatCompletion.choices[0]?.message?.content || "Щось пішло не так, локшина злиплася.";
@@ -133,40 +188,14 @@ bot.on('text', async (ctx) => {
 bot.launch();
 console.log("Бот успішно запущений");
 
-// export default async (req, res) => {
-//   try {
-//     if (req.method === 'POST') {
-//       await bot.handleUpdate(req.body, res);
-//     } else {
-//       res.status(200).send('Бот працює!');
-//     }
-//   } catch (err) {
-//     console.error('Помилка Webhook:', err);
-//     res.status(500).send('Internal Server Error');
-//   }
-// };
-
 
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
 
 
-bot.start((ctx) => {
-  ctx.reply('🍜 Вітаю у Генераторі Локшини!\nЯ допоможу тобі вигадати алібі, у яке ніхто не повірить, але всі запам\'ятають.\n\nОбери режим:', modeKeyboard);
-});
-bot.help((ctx) => {
-  ctx.reply('🍜 Генератор Локшини — твій персональний адвокат у справах провалених дедлайнів та прогуляних пар. Вибирай режим (від Токсика до Філософа) та виходь сухим із води.\n\nПросто обери режим і напиши мені питання для виправдання (наприклад: "чому не прийшов на пару?")');
-});
-bot.command('mode', (ctx) => {
-  ctx.reply('Оберіть режим:', modeKeyboard);
-});
-bot.command('reset', (ctx) => {
-  delete userSessions[ctx.from.id];
-  ctx.reply('✅ Режим скинуто. Виберіть новий режим:', modeKeyboard);
-});
-bot.command('help', (ctx) => {
-  ctx.reply('🍜 Генератор Локшини — твій персональний адвокат у справах провалених дедлайнів та прогуляних пар. Вибирай режим (від Токсика до Філософа) та виходь сухим із води.\n\nПросто обери режим і напиши мені питання для виправдання (наприклад: "чому не прийшов на пару?")');
-});
+
+
+
 bot.on('sticker', (ctx) => {
   ctx.reply('❌ Вибач, це дуже крутий стікер, але я не можу його використовувати. Будь ласка, виберіть режим з меню нижче:', modeKeyboard);
 });
